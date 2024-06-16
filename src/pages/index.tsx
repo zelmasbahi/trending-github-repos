623ca6b1d5af7f62be246b9f8860bf9f6ed99dae
zelmasbahi <z.elmasbahi@yahoo.com>
2024-06-16 12:17:26 +0200

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,90 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import type { InferGetStaticPropsType, GetStaticProps } from "next";
+import { RepoProps } from "@/models";
+import { getRepos } from "@/services";
+import { ReposList, LangFilter } from "@/components";
+import { leftJoin, getFilterOptions } from "@/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export const getStaticProps: GetStaticProps<{
+  repos: RepoProps[];
+}> = async () => {
+  const res = await getRepos();
+  return { props: { repos: res } };
+};
+
+export default function Home({
+  repos,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const [starredRepos, setStarredRepos] = useState<RepoProps[]>([]);
+  const [showStarred, setShowStarred] = useState<boolean>(false);
+
+  const [languageOptions, setLanguageOptions] = useState<string[]>([]);
+  const [selectedLanguages, setSelectedLanguages] = useState<string[]>([]);
+
+  const [filteredRepos, setFilteredRepos] = useState<RepoProps[]>(repos || []);
+
+  useEffect(() => {
+    const savedStarredRepos = JSON.parse(
+      localStorage.getItem("myStarred") || "[]"
+    ) as RepoProps[];
+    setStarredRepos(savedStarredRepos);
+  }, []);
+
+  useEffect(() => {
+    const updatedRepos = showStarred
+      ? starredRepos
+      : leftJoin(repos, starredRepos, "id");
+
+    const filtered =
+      selectedLanguages.length > 0
+        ? updatedRepos.filter((repo) =>
+            selectedLanguages.includes(repo.language as string)
+          )
+        : updatedRepos;
+
+    setFilteredRepos(filtered);
+
+    const availableLanguages = getFilterOptions(updatedRepos);
+    setLanguageOptions(availableLanguages);
+  }, [selectedLanguages, repos, starredRepos, showStarred]);
+
+  useEffect(() => {
+    setSelectedLanguages([]);
+  }, [showStarred]);
+
+  const starRepo = (repo: RepoProps) => {
+    const newMyStarred = starredRepos.some((r) => r.id === repo.id)
+      ? starredRepos.filter((r) => r.id !== repo.id)
+      : [...starredRepos, { ...repo, starred: true }];
+
+    setStarredRepos(newMyStarred);
+    localStorage.setItem("myStarred", JSON.stringify(newMyStarred));
+  };
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <h1 className="text-3xl">Last week popular repos on Github</h1>
+      <div className="p-4 w-full">
+        <button
+          className="float-right"
+          onClick={() => setShowStarred(!showStarred)}
+        >
+          {showStarred ? "View all" : "View my starred"}
+        </button>
+        <div className="w-3/4">
+          <LangFilter
+            selectedLanguages={selectedLanguages}
+            setSelectedLanguages={setSelectedLanguages}
+            languages={languageOptions}
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <ReposList items={filteredRepos} starRepo={starRepo} />
     </main>
   );
 }
